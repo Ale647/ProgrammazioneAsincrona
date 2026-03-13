7bf87d7266f7093f99ed35c3cb98d2c0cab03111
ale <alessandro.bianchini.stud@ispascalcomandini.it>
2026-03-13 09:51:10 +0100

    salvataggio progetto

--- ProgrammazioneAsincrona/ProgrammazioneAsincrona/MainWindow.xaml.cs
+++ ProgrammazioneAsincrona/ProgrammazioneAsincrona/MainWindow.xaml.cs
@@ -58,12 +58,16 @@ namespace ProgrammazioneAsincrona
         {
             if (int.TryParse(TxBox.Text, out int tmp) && tmp >= 1)
             {
+<<<<<<< Updated upstream
                 if(tmp <= dimensione && lunParola < dimensione)
                 {
                     LstBox.Items.Add("");
                     numParole++;
                     lunParola = 0;
                 }
+=======
+
+>>>>>>> Stashed changes
 
                 dimensione = tmp;
             }
